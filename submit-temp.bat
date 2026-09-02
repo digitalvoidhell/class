decb4c0cb1d2ad138c219caa81a8fbf74ec5cd83
digitalvoidhell <digitalvoidhell@hotmail.com>
2026-09-02 07:53:12 +0600

    Auto commit

--- submit-temp.bat
+++ submit-temp.bat
@@ -21,7 +21,7 @@ REM ============================================
 set "REPO=git@github.com:digitalvoidhell/class.git"
 set "BRANCH=main"
 REM set "SSH_KEY=%USERPROFILE%\.ssh\id_ed25519"
-set "SSH_KEY=..\id_ed25519"
+set "SSH_KEY=.\id_ed25519"
 
 REM ============================================
 REM Check Git repository
