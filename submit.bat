34e9ba147b920416c216d58775cab8b51b863d60
digitalvoidhell <digitalvoidhell@hotmail.com>
2026-09-02 06:53:31 +0600

    Backup: Wed 09/02/2026  6:53:30.80

--- submit.bat
+++ submit.bat
@@ -1,15 +1,22 @@
 @echo off
 
-:: ১. টাইমস্ট্যাম্প সেট করা
+:: ১. কমিত মেসেজের জন্য বর্তমান সময় নেওয়া
 set TIMESTAMP=%date% %time%
 echo === Starting Git Push (%TIMESTAMP%) ===
 
-:: ২. ফাইল যুক্ত ও সেভ করা
+:: ২. ফাইল যুক্ত ও লোকালি সেভ করা
 git add .
 git commit -m "Backup: %TIMESTAMP%"
 
-:: ৩. আপনার গিটহাব ইউজারনেম এবং পাসওয়ার্ড হিসেবে টোকেন দিয়ে সরাসরি পুশ
-git push https://github.com main
+:: ৩. লোকাল গিটের ভেতর আপনার টোকেনটি সাময়িকভাবে পুশ করার জন্য সেট করা
+git config --local credential.helper ""
+git remote set-url origin https://github.com
+
+:: ৪. গিটহাবে পুশ করা (এখানে উইন্ডোজ আর ইউআরএল কাটতে পারবে না)
+git push origin main
+
+:: ৫. কাজ শেষে লোকাল ইউআরএল আবার সাধারণ অবস্থায় ফিরিয়ে নেওয়া
+git remote set-url origin https://github.com
 
 echo === Done! ===
 pause
