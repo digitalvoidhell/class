5c9e993054a781eb9f22c02c959337c518ac8cd3
digitalvoidhell <digitalvoidhell@hotmail.com>
2026-09-02 06:30:58 +0600

    Backup: Wed 09/02/2026  6:30:58.33

--- submit.bat
+++ submit.bat
@@ -1,30 +1,31 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
+
 :: 1. SET YOUR GITHUB CONFIGURATION VARIABLES
-set GITHUB_USER=digitalvoidhell
-set GITHUB_TOKEN=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
-set REPO_OWNER=digitalvoidhell
-set REPO_NAME=class
-set BRANCH_NAME=main
+set "GITHUB_USER=digitalvoidhell"
+set "GITHUB_TOKEN=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"
+set "REPO_OWNER=digitalvoidhell"
+set "REPO_NAME=class"
+set "BRANCH_NAME=main"
 
 :: 2. Get current date and time for the commit message
-for /f "tokens=2-4 delims=/ " %%a in ('date /t') do set mydate=%%c-%%a-%%b
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do set mytime=%%a-%%b
-set TIMESTAMP=%date% %time%
+set "TIMESTAMP=%date% %time%"
 
-echo === Starting Git Push (%TIMESTAMP%) ===
+echo === Starting Git Push (!TIMESTAMP!) ===
 
-:: 3. Temporarily update the URL to embed your token
-git remote set-url origin https://%GITHUB_USER%:%GITHUB_TOKEN%@://github.com
+:: 3. Temporarily update the URL to embed your token securely using Delayed Expansion
+git remote set-url origin https://!GITHUB_USER!:!GITHUB_TOKEN!@://github.com
 
 :: 4. Stage and commit all changes
 git add .
-git commit -m "Backup: %TIMESTAMP%"
+git commit -m "Backup: !TIMESTAMP!"
 
 :: 5. Push to GitHub
-git push origin %BRANCH_NAME%
+git push origin !BRANCH_NAME!
 
-:: 6. Clean up the URL in local settings so your token stays safe
+:: 6. Clean up the URL in local settings so your token isn't stored in plain text configs
 git remote set-url origin https://://github.com
 
 echo === Done! ===
 pause
+ENDLOCAL
